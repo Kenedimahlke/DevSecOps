ab48eeff754b147c58ceb3d609b346974dc9babd
Kenedi Mahlke <127451564+Kenedimahlke@users.noreply.github.com>
2026-09-18 20:09:32 -0300

    Corrige vulnerabilidades SAST

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import sqlite3
+import os
+
 
 def conectar_banco():
-    # ERRO DE SEGURANÇA 1 (SAST): Credencial exposta no código.
-    # O SAST vai detectar a palavra 'password' recebendo uma string fixa.
-    db_password = "super_senha_secreta_123"
-    print(f"Conectando ao banco de forma insegura com a senha: {db_password}") #mera simulação
+    senha_banco = os.getenv("DB_PASSWORD", "senha_padrao_local")
+
+    print("Conexão estabelecida de forma segura.")
+
 
 def buscar_usuario(nome_usuario):
-    # ERRO DE SEGURANÇA 2 (SAST): Risco crítico de SQL Injection.
-    conn = sqlite3.connect('banco_exemplo.db')
+    conn = sqlite3.connect("banco_exemplo.db")
     cursor = conn.cursor()
-    query = "SELECT * FROM usuarios WHERE nome = '" + nome_usuario + "'"
-    cursor.execute(query)
+
+    query = "SELECT * FROM usuarios WHERE nome = ?"
+
+    cursor.execute(query, (nome_usuario,))
+
     return cursor.fetchall()
+
 
 if __name__ == "__main__":
     conectar_banco()
-
-
-# teste pipeline
-# aaaaaa
